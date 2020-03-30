46feebf780ea0e1e9c1afec44e8263ca0568f2a3
Serchi <sergionavarro@minimizan.com>
2020-03-30 20:37:55 +0200

    Update pagina2.php

--- pagina2.php
+++ pagina2.php
@@ -52,7 +52,7 @@
     $partidas = BD::obtieneEquipos($idJuego); 
     // si no hay muestra mensaje
     if ($partidas==''){
-        $smarty->assign('mijuego',"este juego no tiene partidas");
+        $smarty->assign('mijuego',"juego no seleccionado o no tiene partidas");
     }else{
         $smarty->assign('mijuego', $mijuego);
     }
